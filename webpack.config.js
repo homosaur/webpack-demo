111337052887303c6c84f9a64814075ce3ca3b18
Paul Graham <pgraham@hlkagency.com>
2017-06-01 03:54:18 -0500

    source maps for CSS

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,17 @@ module.exports = {
           use: [
             {
               loader: 'css-loader',
-              options: { importLoaders: 1 }
+              options: {
+                importLoaders: 1,
+                sourceMap: true
+              }
             },
-            'postcss-loader'
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            }
           ]
         })
       }
