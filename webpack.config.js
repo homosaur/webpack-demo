5aac7e5fd8582f80b6e97698a30a9e1cea165a67
Paul Graham <pgraham@hlkagency.com>
2017-06-01 03:41:59 -0500

    add postcss loader to chain

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,11 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           'style-loader',
-          'css-loader'
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          'postcss-loader'
         ]
       }
     ]
