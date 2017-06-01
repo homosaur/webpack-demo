f19bf12d891acfb4eb332601f97f461ff18d2323
Paul Graham <pgraham@hlkagency.com>
2017-06-01 02:53:17 -0500

    configure babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
   entry: './src/entry.js',
   output: {
     filename: './dist/bundle.js'
